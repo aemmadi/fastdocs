f975dfe332cbe9acd0ec081299e5f385421fe12f
Anirudh Emmadi <kanna6501@gmail.com>
2020-08-04 23:12:38 -0500

    feat: generates working html file with docsify

--- app.js
+++ app.js
@@ -1,9 +1,8 @@
-const express = require("express");
 const axios = require("axios");
-const fs = require("fs");
-const jsdom = require("jsdom");
-const { JSDOM } = jsdom;
+const express = require("express");
 const { response } = require("express");
+const fs = require("fs");
+
 const app = express();
 const port = 3000;
 
@@ -14,6 +13,7 @@ app.get("/:user/:repo", (req, res) => {
   const repo = req.params.repo;
   getReadme(user, repo);
 
+  // Gets readme contents and generates /docs/<files> with docsify enabled to render
   async function getReadme(user, repo) {
     const readme = await axios.get(
       `https://api.github.com/repos/${user}/${repo}/readme`
@@ -21,14 +21,21 @@ app.get("/:user/:repo", (req, res) => {
     const readmeUrl = readme.data.download_url;
     const readmeData = await axios.get(readmeUrl);
 
-    fs.writeFile(`${user}-${repo}.md`, readmeData.data, function (err) {
+    fs.mkdirSync("./docs/");
+
+    fs.writeFile(`./docs/${user}-${repo}.md`, readmeData.data, function (err) {
       if (err) throw err;
       console.log("Saved!");
     });
 
     const htmlData = `<!DOCTYPE html><html lang="en"><head><meta charset="UTF-8"/><title>${repo}</title><meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1"/><meta name="description" content="Description"/> <meta name="viewport" content="width=device-width, initial-scale=1.0, minimum-scale=1.0"/> <link rel="stylesheet" href="//cdn.jsdelivr.net/npm/docsify@4/lib/themes/vue.css"/> </head> <body> <div id="app"></div><script>window.$docsify={name: '${repo}', repo: '${user}/${repo}', homepage: '${user}-${repo}.md'}; </script> <script src="//cdn.jsdelivr.net/npm/docsify@4"></script> </body></html>`;
 
-    fs.writeFile(`${user}-${repo}.html`, htmlData, function (err) {
+    fs.writeFile("./docs/.nojekyll", "", function (err) {
+      if (err) throw err;
+      console.log("Generated nojekyll");
+    });
+
+    fs.writeFile(`./docs/index.html`, htmlData, function (err) {
       if (err) throw err;
       console.log("Generated HTML!");
     });
