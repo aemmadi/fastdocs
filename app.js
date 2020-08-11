42e592b3850ecb4b27717e9ac83efc8766999b06
Anirudh Emmadi <kanna6501@gmail.com>
2020-08-11 11:00:28 -0500

    feat(#1): serves docsify docs at /:user/:repo endpoint

--- app.js
+++ app.js
@@ -6,11 +6,9 @@ const fs = require("fs");
 const app = express();
 const port = 3000;
 
-app.use('/docs', express.static('docs'))
-
 app.get("/", (req, res) => res.send("Hello World!"));
 
-app.get("/:user/:repo", (req, res) => {
+app.get("/:user/:repo/compile", (req, res) => {
   const user = req.params.user;
   const repo = req.params.repo;
   getReadme(user, repo);
@@ -23,29 +21,34 @@ app.get("/:user/:repo", (req, res) => {
     const readmeUrl = readme.data.download_url;
     const readmeData = await axios.get(readmeUrl);
 
-    fs.mkdirSync("./docs/");
+    fs.mkdirSync(`./${user}-${repo}/`);
 
-    fs.writeFile(`./docs/${user}-${repo}.md`, readmeData.data, function (err) {
+    fs.writeFileSync(`./${user}-${repo}/README.md`, readmeData.data, function (
+      err
+    ) {
       if (err) throw err;
       console.log("Saved!");
     });
 
-    const htmlData = `<!DOCTYPE html><html lang="en"><head><meta charset="UTF-8"/><title>${repo}</title><meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1"/><meta name="description" content="Description"/> <meta name="viewport" content="width=device-width, initial-scale=1.0, minimum-scale=1.0"/> <link rel="stylesheet" href="//cdn.jsdelivr.net/npm/docsify@4/lib/themes/vue.css"/> </head> <body> <div id="app"></div><script>window.$docsify={name: '${repo}', repo: '${user}/${repo}', homepage: '${user}-${repo}.md'}; </script> <script src="//cdn.jsdelivr.net/npm/docsify@4"></script> </body></html>`;
+    const htmlData = `<!DOCTYPE html><html lang="en"><head><meta charset="UTF-8"/><title>${repo}</title><meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1"/><meta name="description" content="Description"/> <meta name="viewport" content="width=device-width, initial-scale=1.0, minimum-scale=1.0"/> <link rel="stylesheet" href="//cdn.jsdelivr.net/npm/docsify@4/lib/themes/vue.css"/> </head> <body> <div id="app"></div><script>window.$docsify={name: '${repo}', repo: '${user}/${repo}'}; </script> <script src="//cdn.jsdelivr.net/npm/docsify@4"></script> </body></html>`;
 
-    fs.writeFile("./docs/.nojekyll", "", function (err) {
+    fs.writeFileSync(`./${user}-${repo}/.nojekyll`, "", function (err) {
       if (err) throw err;
       console.log("Generated nojekyll");
     });
 
-    fs.writeFile(`./docs/index.html`, htmlData, function (err) {
+    fs.writeFileSync(`./${user}-${repo}/index.html`, htmlData, function (err) {
       if (err) throw err;
       console.log("Generated HTML!");
     });
-
-    res.send(readmeData.data);
+    await serveDocs(user, repo);
+    res.send(`Successfully Compiled Docs! View them at /${user}/${repo}`);
   }
 });
 
+async function serveDocs(user, repo) {
+  app.use(`/${user}/${repo}`, express.static(`${user}-${repo}`));
+}
 
 app.listen(port, () =>
   console.log(`Example app listening at http://localhost:${port}`)
