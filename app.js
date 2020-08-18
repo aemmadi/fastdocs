c793ae4c01ce1873c28780c53763a0fc4cf171af
Anirudh Emmadi <kanna6501@gmail.com>
2020-08-18 16:12:57 -0500

    chore: refactored function names, added basic error handling

--- app.js
+++ app.js
@@ -1,15 +1,9 @@
 const express = require("express");
 
 const util = require("./util");
+
 const app = express();
 const port = 3000;
-
-let defaultConfig = {
-  name: "",
-  description: "Description",
-  plugins: [],
-  theme: "",
-};
 
 app.get("/", (req, res) => res.send("Hello World!"));
 
@@ -17,10 +11,16 @@ app.get("/:user/:repo/compile", (req, res) => {
   const user = req.params.user;
   const repo = req.params.repo;
 
-  util.getReadme(user, repo).then(() => {
-    util.serveDocs(app, user, repo);
-    res.send(`Successfully Compiled Docs! View them at /${user}/${repo}`);
-  });
+  try {
+    util.compileDocs(user, repo).then(() => {
+      util.serveDocs(app, user, repo);
+      res.send(`Successfully Compiled Docs! View them at /${user}/${repo}`);
+    });
+  } catch (error) {
+    res.send(
+      `Oops. We couldn't render these docs. Check your config if you have one and try again!`
+    );
+  }
 });
 
 app.listen(port, () =>
