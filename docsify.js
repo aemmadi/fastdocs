ab32a7092f15e1d487ed22ae7135c9e4d40db6ae
Anirudh Emmadi <kanna6501@gmail.com>
2020-08-19 11:11:42 -0500

    chore: reduced conditional return statements (#14)

--- docsify.js
+++ docsify.js
@@ -5,22 +5,19 @@ function generateHTML(user, repo) {
 
 // Generated docsify enabled html with config and plugins
 function generateHtmlWithConfig(config) {
-  if (config.enablePlugins) {
-    let ga = ""; // google analytics code
-    let search = ""; // search path
+  let ga = ""; // google analytics code
+  let search = ""; // search path
 
+  if (config.enablePlugins) {
     if (config.plugins.ga) {
       ga = `, ga: ${config.gaCode}`;
     }
     if (config.plugins.search) {
       search = ', search: ["/"]';
     }
-
-    return `<!DOCTYPE html><html lang="en"><head><meta charset="UTF-8"/><title>${config.repo}</title><meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1"/><meta name="description" content="${config.description}"/> <meta name="viewport" content="width=device-width, initial-scale=1.0, minimum-scale=1.0"/> <link rel="stylesheet" href="//cdn.jsdelivr.net/npm/docsify@4/lib/themes/vue.css"/> </head> <body> <div id="app"></div><script>window.$docsify={name: '${config.repo}', repo: '${config.user}/${config.repo}'${ga}${search}}; </script> <script src="//cdn.jsdelivr.net/npm/docsify@4"></script>${config.plugins.tags}</body></html>`;
   }
 
-  // If no plugins, but config exists for description
-  return `<!DOCTYPE html><html lang="en"><head><meta charset="UTF-8"/><title>${config.repo}</title><meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1"/><meta name="description" content="${config.description}"/> <meta name="viewport" content="width=device-width, initial-scale=1.0, minimum-scale=1.0"/> <link rel="stylesheet" href="//cdn.jsdelivr.net/npm/docsify@4/lib/themes/vue.css"/> </head> <body> <div id="app"></div><script>window.$docsify={name: '${config.repo}', repo: '${config.user}/${config.repo}'}; </script> <script src="//cdn.jsdelivr.net/npm/docsify@4"></script> </body></html>`;
+  return `<!DOCTYPE html><html lang="en"><head><meta charset="UTF-8"/><title>${config.repo}</title><meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1"/><meta name="description" content="${config.description}"/> <meta name="viewport" content="width=device-width, initial-scale=1.0, minimum-scale=1.0"/> <link rel="stylesheet" href="//cdn.jsdelivr.net/npm/docsify@4/lib/themes/vue.css"/> </head> <body> <div id="app"></div><script>window.$docsify={name: '${config.repo}', repo: '${config.user}/${config.repo}'${ga}${search}}; </script> <script src="//cdn.jsdelivr.net/npm/docsify@4"></script>${config.plugins.tags}</body></html>`;
 }
 
 module.exports = { generateHTML, generateHtmlWithConfig };
