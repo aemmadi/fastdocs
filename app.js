e8daa7f48dda0439a04c9afdb1cd8840eae196d0
Anirudh Emmadi <kanna6501@gmail.com>
2020-08-20 11:11:26 -0500

    feat: upload and download docs from s3 (#16)

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 
 const util = require("./util");
+const { awsUpload, awsDownload } = require("./aws");
 
 const app = express();
 const port = process.env.PORT || 3000;
@@ -12,9 +13,19 @@ app.get("/:user/:repo/compile", (req, res) => {
   const repo = req.params.repo;
 
   util.compileDocs(user, repo).then(() => {
-    util.serveDocs(app, user, repo);
+    // util.serveDocs(app, user, repo);
+    awsUpload(user, repo);
+
     res.send(`Successfully Compiled Docs! View them at /${user}/${repo}`);
   });
+});
+
+app.get("/:user/:repo", (req, res) => {
+  const user = req.params.user;
+  const repo = req.params.user;
+
+  awsDownload(user, repo);
+  util.serveDocs(app, user, repo);
 });
 
 app.listen(port, () => console.log(`Deployed on port: ${port}`));
